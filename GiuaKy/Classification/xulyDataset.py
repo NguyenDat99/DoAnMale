d60f2648bebef7a1d5a6ec2ba079abbbe4105fd0
NguyenDat99 <donguyen5548@gmail.com>
2019-04-01 23:06:00 +0700

    Thêm vài hàm chuyển đổi

--- GiuaKy/Classification/xulyDataset.py
+++ GiuaKy/Classification/xulyDataset.py
@@ -2,6 +2,33 @@
 import pandas as pd
 x_training=[]
 y_training=[]
+class bangAnhXa:
+    def __init__(self,giatriCu,giatriMoi):
+        self.giatriCu=giatriCu
+        self.giatriMoi=giatriMoi
+def xoaTrung(a):
+    b=[]
+    for i in a:
+        if i not in b:
+            b.append(i)
+    return b
+def ganSoPhanLoai(mangXoaTrung):
+    b=[]
+    for i in range(len(mangXoaTrung)):
+        b.append([mangXoaTrung[i],i])
+    return b
+def chuyen_dac_truong_sang_so(dac_trung_cu,mang_ganSoPhanLoai):
+    a=[]
+    for i in dac_trung_cu:
+        for j in mang_ganSoPhanLoai:
+            if i==j[0]:
+                a.append([j[1]])
+    return a;
+
+
+
+
+
 
 DauVao=pd.read_csv('./dataset/dataset_for_classification.csv',encoding='utf-8')
 tuoi=(DauVao['tuoi'].values).tolist()
@@ -26,3 +53,7 @@ lai_suat_3thang=(DauVao['lai_suat_3thang'].values).tolist()
 so_luong_nhan_vien=(DauVao['so_luong_nhan_vien'].values).tolist()
 
 y_training=(DauVao['label'].values).tolist()
+
+
+
+print(xoaTrung(tuoi))
