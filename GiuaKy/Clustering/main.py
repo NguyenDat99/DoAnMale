e4efbaaab4bf514ed51f4fd84c3cc0f0714cbc07
NguyenDat99 <donguyen5548@gmail.com>
2019-04-27 15:33:35 +0700

    Km

--- GiuaKy/Clustering/main.py
+++ GiuaKy/Clustering/main.py
@@ -5,12 +5,12 @@ import HC
 
 
             # KMeans
-KMeans.veTimSoCluster(['luong_hao_xang',
-'so_luong_xi_lanh','the_tich_dong_co',
-'ma_luc','ty_le_truc_sau','khoi_luong_xe',
-'gia_toc_xe','loai_xy_lanh_dong_co',
-'loai_truyen_dong','so_luong_banh_rang',
-'so_luong_bo_che_hoa_khi'],1,7)
+# KMeans.veTimSoCluster(['luong_hao_xang',
+# 'so_luong_xi_lanh','the_tich_dong_co',
+# 'ma_luc','ty_le_truc_sau','khoi_luong_xe',
+# 'gia_toc_xe','loai_xy_lanh_dong_co',
+# 'loai_truyen_dong','so_luong_banh_rang',
+# 'so_luong_bo_che_hoa_khi'],1,7)
 
 # k=km.KMean(['luong_hao_xang',
 # 'so_luong_xi_lanh','the_tich_dong_co',
