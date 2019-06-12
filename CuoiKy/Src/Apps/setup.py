670e82cdb945d9f4d23b2c19f1f885636b2e5afb
NguyenDat99 <donguyen5548@gmail.com>
2019-06-12 19:10:27 +0700

    Cap Nhat Cython

---
+++ CuoiKy/Src/Apps/setup.py
@@ -0,0 +1,7 @@
+from distutils.core import setup
+from Cython.Build import cythonize
+
+setup(ext_modules=cythonize('App.pyx'))
+setup(ext_modules=cythonize('../BUS/Multivariable_Regression.pyx'))
+setup(ext_modules=cythonize('../DAO/DataProcessing.pyx'))
+setup(ext_modules=cythonize('../DTO/DataAdapter.pyx'))
